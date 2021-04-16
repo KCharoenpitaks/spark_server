d1b724e599303a4a26d1032cc08b22e221caf868
unknown <OH@DESKTOP-5MM6LT5.thaibdc.local>
2021-04-16 15:36:54 +0700

    test

--- build.sh
+++ build.sh
@@ -9,23 +9,33 @@ JUPYTERLAB_VERSION="2.1.5"
 docker build \
   -f cluster-base.Dockerfile \
   -t cluster-base .
+docker tag cluster-base megadarkparticle/cluster-base
+docker push megadarkparticle/cluster-base
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg hadoop_version="${HADOOP_VERSION}" \
   -f spark-base.Dockerfile \
   -t spark-base .
+docker tag spark-base megadarkparticle/spark-base
+docker push megadarkparticle/spark-base
 
 docker build \
   -f spark-master.Dockerfile \
   -t spark-master .
+docker tag spark-master megadarkparticle/spark-master
+docker push megadarkparticle/spark-master
 
 docker build \
   -f spark-worker.Dockerfile \
   -t spark-worker .
+docker tag spark-worker megadarkparticle/spark-worker
+docker push megadarkparticle/spark-worker
 
 docker build \
   --build-arg spark_version="${SPARK_VERSION}" \
   --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" \
   -f jupyterlab.Dockerfile \
   -t jupyterlab .
+docker tag jupyterlab megadarkparticle/jupyterlab
+docker push megadarkparticle/jupyterlab
